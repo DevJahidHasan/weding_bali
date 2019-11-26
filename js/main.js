f65c81cd6053f6412bd5895bda3a753df9370b9b
jahid <fjahidhasan12@gmail.com>
2019-11-26 14:38:32 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,188 @@
+
+(function ($) {
+    "use strict";
+
+
+
+    // -------------------------------------------------------------
+    //  Slick Slider
+    // -------------------------------------------------------------  
+
+	
+	$(document).ready(function() {
+		(function( $ ) {
+
+    //Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#sg-carousel'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  
+   
+	
+})(jQuery);	
+	   
+	});
+
+    // -------------------------------------------------------------
+    //  Smooth scroll
+    // -------------------------------------------------------------  
+
+// Smooth scroll
+   // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+    // -------------------------------------------------------------
+    // headroom
+    // -------------------------------------------------------------
+		// grab an element
+        var myElement = document.querySelector(".headroom");
+        // construct an instance of Headroom, passing the element
+        var headroom  = new Headroom(myElement);
+        // initialise
+        headroom.init(); 
+
+
+    // -------------------------------------------------------------
+    // counterup
+    // -------------------------------------------------------------  
+
+            $('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
+
+    // -------------------------------------------------------------
+    // carousel
+    // -------------------------------------------------------------  
+	
+	  //Set the carousel options
+		  $('#quote-carousel').carousel({
+		    pause: true,
+		    interval: 4000,
+		  });
+
+	// -------------------------------------------------------------
+    // Wow
+    // ------------------------------------------------------------- 
+	 wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+
+
+
+})(jQuery);
+
+   // ranger start here
+$(document).ready(function () {
+        $("#slider").slider({
+            range: "min",
+            animate: true,
+            value: 1,
+            min: 5000,
+            max: 45000,
+            step: 500,
+            slide: function (event, ui) {
+                update(1, ui.value); //changed
+            }
+        });
+
+        //Added, set initial value.
+        $("#amount").val(0);
+        $("#amount-label").text(0);
+
+
+        update();
+    });
+
+    //changed. now with parameter
+    function update(slider, val) {
+        //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+        var $amount = slider == 1 ? val : $("#amount").val();
+
+        /* commented
+         $amount = $( "#slider" ).slider( "value" );
+         $duration = $( "#slider2" ).slider( "value" );
+         */
+
+        $("#amount").val($amount);
+        $("#amount-label").text($amount);
+
+        $('#slider a').html('<label>' + $amount + '</label><div class="ui-slider-label-inner"></div>');
+    }
+ 
---
+++ order_now/js/event/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+    "use strict";
+	
+	$(document).ready(function() {
+		(function( $ ) {
+
+    //Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#sg-carousel'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  
+   
+	
+})(jQuery);	
+	
+       
+	   
+	});
+	
+	
+	
+})(jQuery);
+  // ranger start here
+$(document).ready(function () {
+        $("#slider").slider({
+            range: "min",
+            animate: true,
+            value: 1,
+            min: 5000,
+            max: 45000,
+            step: 500,
+            slide: function (event, ui) {
+                update(1, ui.value); //changed
+            }
+        });
+
+        //Added, set initial value.
+        $("#amount").val(0);
+        $("#amount-label").text(0);
+
+
+        update();
+    });
+
+    //changed. now with parameter
+    function update(slider, val) {
+        //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+        var $amount = slider == 1 ? val : $("#amount").val();
+
+        /* commented
+         $amount = $( "#slider" ).slider( "value" );
+         $duration = $( "#slider2" ).slider( "value" );
+         */
+
+        $("#amount").val($amount);
+        $("#amount-label").text($amount);
+
+        $('#slider a').html('<label>' + $amount + '</label><div class="ui-slider-label-inner"></div>');
+    }
